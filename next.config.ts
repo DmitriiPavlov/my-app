10042536617a03340069aebc9d42bcd0a1e781d3
smartbison <121305911+smartbison@users.noreply.github.com>
2024-11-06 19:57:33 -0500

    yay

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+},
+typescript: {
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: true,
+},
+experimental: {
+  appDir: true,
+  devIndicators: {
+    buildActivity: true,
+    appIsrStatus: true,
+  },
+},
 };
 
 export default nextConfig;
